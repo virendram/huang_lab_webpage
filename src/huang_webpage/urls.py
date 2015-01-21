f2b4f13f6bc748e6d82ec4e77e632e2e062d24f7
virendram <virendra.mishra@gmail.com>
2015-01-21 00:40:30 -0600

    Added a lot of important html files

--- src/huang_webpage/urls.py
+++ src/huang_webpage/urls.py
@@ -11,7 +11,11 @@ urlpatterns = patterns('',
     # Examples:
      url(r'^$', 'signups.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^download/$', 'signups.views.download', name='download'),
+    url(r'^aboutus/$', 'signups.views.aboutus', name='aboutus'),
+    url(r'^publications/$', 'signups.views.publications', name='publications'),
+    url(r'^research/$', 'signups.views.research', name='research'),
+    url(r'^contactus/$', 'signups.views.contactus', name='contactus'),
     url(r'^admin/', include(admin.site.urls)),
 )
 
--- src/signups/views.py
+++ src/signups/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, render_to_response, RequestContext
+from django.shortcuts import render, render_to_response, RequestContext, HttpResponseRedirect
+from django.contrib import messages
 
 # Create your views here.
 
@@ -10,7 +11,38 @@ def home(request):
     if form.is_valid():
         save_it=form.save(commit=False)
         save_it.save()
-        
+        messages.success(request,'Thank You! You can download the file now!')
+        return HttpResponseRedirect('/download')
     return render_to_response("signup.html",
+                              locals(),
+                              context_instance=RequestContext(request))
+
+def download(request):
+    
+    return render_to_response("downloads.html",
+                              locals(),
+                              context_instance=RequestContext(request))
+
+def aboutus(request):
+    
+    return render_to_response("about_us.html",
+                              locals(),
+                              context_instance=RequestContext(request))
+
+def publications(request):
+    
+    return render_to_response("publications.html",
+                              locals(),
+                              context_instance=RequestContext(request))
+
+def research(request):
+    
+    return render_to_response("research.html",
+                              locals(),
+                              context_instance=RequestContext(request))
+
+def contactus(request):
+    
+    return render_to_response("contact_us.html",
                               locals(),
                               context_instance=RequestContext(request))
